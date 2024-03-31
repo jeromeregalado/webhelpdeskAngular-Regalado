532628e1e1221a406aa4578a59a991917a2b154e
jeromeregalado <jregalado@exist.com>
2024-03-31 15:34:15 +0800

    fixed delete and update buttons.

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
-import{FormsModule} from '@angular/forms'
 
 import { Router } from '@angular/router';
 @Component({
@@ -12,7 +11,7 @@ import { Router } from '@angular/router';
 export class EmployeeListComponent {
 
   employees: Employee[];
-  EnteredID!:number;
+  EnteredEmployeeNumber!:number;
 
   constructor(private employeeService: EmployeeService,  private router: Router) {
     this.employees=[];
@@ -21,48 +20,34 @@ export class EmployeeListComponent {
 
   ngOnInit(): void {
 
-    // this.employees = [
-    //   { "id":1,fname: 'John', lname: 'Doe', email: 'john@example.com', salary: 50000, department: 'IT', designation: 'Developer' },
-
-    // ];
-
     this.getEmployees();
   }
 
-
   goToEmployee(){
-
-
-    console.log(this.EnteredID);
-    this.router.navigate(['details-of-employee',this.EnteredID]);
+    console.log(this.EnteredEmployeeNumber);
+    this.router.navigate(['details-of-employee',this.EnteredEmployeeNumber]);
   }
 
   getEmployees(){
     this.employeeService.getEmployeesList().subscribe(data => {this.employees = data;});
-
-
   }
 
   updateEmployee(id: number){
-    this.router.navigate(['updating-by-id', id]);
+    this.router.navigate(['updating-by-employeeNumber/', id]);
   }
 
-
-
-
-  deleteEmployee(id: number){
-
-    if(confirm("Are you sure to delete Employee ID: "+id)){
-    this.employeeService.deleteEmployee(id).subscribe( data => {
+  deleteEmployee(employeeNumber: number){
+    if(confirm("Are you sure to delete Employee Number: " + employeeNumber)){
+    this.employeeService.deleteEmployee(employeeNumber).subscribe( data => {
       console.log(data);
       this.getEmployees();
-    })}
+    })
+    window.location.reload();
+  }
   }
 
 
   detailsOfEmployee(id: number){
     this.router.navigate(['details-of-employee', id]);
   }
-
-
 }
