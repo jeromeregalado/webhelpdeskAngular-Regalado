b317a36693a73842f683529f3aee45c2288dfa25
Jerome Regalado <jerome.regalado@softvision.com>
2024-04-10 06:55:21 +0800

    removed unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
-import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
-import { FormsModule } from '@angular/forms';
-
-import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
-import { EmployeeShowDetailsComponent } from './employee/employee-show-details/employee-show-details.component';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TicketListComponent } from './ticket/ticket-list/ticket-list.component';
-import { AddTicketComponent } from './ticket/add-ticket/add-ticket.component';
-import { ViewAssignedTicketsComponent } from './employee/view-assigned-tickets/view-assigned-tickets.component';
-import { LoginComponent } from './login/login.component';
 import { HttpInterceptorService } from './login/HttpInterceptorService';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { UpdateTicketComponent } from './ticket/update-ticket/update-ticket.component';
 import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
